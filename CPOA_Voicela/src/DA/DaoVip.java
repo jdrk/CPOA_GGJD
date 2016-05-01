662fe078a34199210320518d3d083a21fa7e21e7
neexs <guillaume.gebavi@yahoo.com>
2016-05-01 13:30:56 +0200

    + Base projet NetBeans
    2 couches métiers,
    1 DAO,
    un début d'interface graphique,
    
    Connexion : SQLDevelopper

---
+++ CPOA_Voicela/src/DA/DaoVip.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DA;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import métier.Vip;
+
+/**
+ *
+ * @author Guillaume
+ */
+public class DaoVip {
+    
+    private final Connection connexion;
+    
+    private DaoVip(Connection connexion) throws SQLException{
+        this.connexion = connexion;
+    }
+    
+    public void lireVip(ArrayList<Vip> lesVip) throws SQLException{
+        String requete = "SELECT * FROM VIP";
+        PreparedStatement pstmt = connexion.prepareStatement(requete);
+        ResultSet rset = pstmt.executeQuery(requete);
+        while (rset.next()) {
+            String num = rset.getString(1);
+            String nom = rset.getString(2);
+            String prenom = rset.getString(3);
+            String civilite = rset.getString(4);
+            String dateNaissance = rset.getString(5);
+            String lieuNaissance = rset.getString(6);
+            String codeRole = rset.getString(7);
+            String codeStatut = rset.getString(8);
+            String nationalite = rset.getString(9);
+            Vip temp = new Vip(num,nom,prenom,civilite,dateNaissance,lieuNaissance,codeRole,codeStatut,nationalite);
+            lesVip.add(temp);
+        }
+        rset.close();
+        pstmt.close();        
+      
+    }
+    
+    public void addVip(Vip vip) throws SQLException{
+        String requete = "INSERT INTO VIP VALUES(?,?,?,?,?,?,?,?,?);";
+        PreparedStatement pstmt = connexion.prepareStatement(requete);
+        pstmt.setString(1, vip.getNumVip());
+        pstmt.setString(2, vip.getNomVip());
+        pstmt.setString(3,vip.getPrenomVip());
+        pstmt.setString(4, vip.getCivilite());
+        pstmt.setString(5, vip.getLieuNaissance());
+        pstmt.setString(6, vip.getLieuNaissance());
+        pstmt.setString(7, vip.getCodeRole());
+        pstmt.setString(8, vip.getCodeStatut());
+        pstmt.setString(9, vip.getNationalite());
+        pstmt.executeUpdate();
+        pstmt.close();
+      
+    }
+    
+    public void delVip(Vip vip) throws SQLException{
+        String requete = "DELETE FROM VIP WHERE NUMVIP = ?;";
+        PreparedStatement pstmt = connexion.prepareStatement(requete);
+        pstmt.setInt(1, Integer.parseInt(vip.getNumVip()) );
+        pstmt.executeUpdate();
+        pstmt.close();
+    }
+    
+}
---
+++ CPOA_Voicela/src/métier/Genre.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package métier;
+
+/**
+ *
+ * @author Guillaume
+ */
+public class Genre {
+    
+    private String idGenre;
+    private String libbeleGenre;
+
+    public Genre(String idGenre, String libbeleGenre) {
+        this.idGenre = idGenre;
+        this.libbeleGenre = libbeleGenre;
+    }
+    
+    public Genre(){
+    }
+
+    public String getIdGenre() {
+        return idGenre;
+    }
+
+    public void setIdGenre(String idGenre) {
+        this.idGenre = idGenre;
+    }
+
+    public String getLibbeleGenre() {
+        return libbeleGenre;
+    }
+
+    public void setLibbeleGenre(String libbeleGenre) {
+        this.libbeleGenre = libbeleGenre;
+    }
+
+    @Override
+    public String toString() {
+        return "Genre{" + "idGenre=" + idGenre + ", libbeleGenre=" + libbeleGenre + '}';
+    }
+    
+    
+}
